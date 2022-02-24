5fbb0d86106dabb7ce075e17c29b7ef063e68d90
Diego Santos <diegodeveloper42@gmail.com>
2022-02-24 16:38:27 -0300

    added side transition in the sessions with js and css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ function toggleMenu() {
   menuToggle.classList.toggle('active');
   navigation.classList.toggle('active');
 }
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if (sections.length) {
+    const windowSection = window.innerHeight * 0.4;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowSection) < 0;
+        if (isSectionVisible)
+          section.classList.add('active');
+        else
+          section.classList.remove('active');
+      })
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
